07509791611b5472a99b9a153cc4df02bf933096
Amine FLE <benchoamino@gmail.com>
2025-04-10 23:10:54 +0200

    Update server.py

--- api/server.py
+++ api/server.py
@@ -86,4 +86,5 @@ def telecharger_docx(nom_fichier):
     return send_from_directory(DOCX_FOLDER, nom_fichier, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
+
